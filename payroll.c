f034e78beff8ffcf117929af25c0f68b2a1c4185
nclarkinwright <49002688+nclarkinwright@users.noreply.github.com>
2020-02-12 15:25:36 -0500

    Payroll done, except for rounding
    Need to fix rounding for hours worked

--- payroll.c
+++ payroll.c
@@ -4,18 +4,41 @@
 * Assignment Two
 */
 
-# include <stdio.h>
-# include <stdlib.h>
-# define MAX_CHARS 30
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
 
-int main(int argc, char* argv[]) {
-    char first_name[MAX_CHARS], last_name[MAX_CHARS];
-    int hours_worked;
+int main(int argc, char* argv[]) 
+{
+    char *line, *first_name, *last_name;
+    size_t buf_size = 0;
+    const char delim[] = {' ','\t','\n','\v','\f','\r'};
+    double hours_worked;
+    int hours_worked_rnded;
     double hourly_rate;
     
-    printf("Enter first name (30 characters max): ");
-    fgets(first_name, MAX_CHARS, stdin);
-    printf("Enter last name (30 characters max): ");
-    fgets(last_name, MAX_CHARS, stdin);
+    printf("Enter first name: ");
+    getline(&line, &buf_size, stdin);
+    first_name = strtok(line, delim);
+    line = NULL;
     
+    printf("Enter last name: ");
+    getline(&line, &buf_size, stdin);
+    last_name = strtok(line, delim);
+    line = NULL;
+
+    printf("Enter hours worked: ");
+    getline(&line, &buf_size, stdin);
+    hours_worked = strtod(line, &line);
+    //hours_worked_rnded = nearbyint(hours_worked);
+    line = NULL;
+    
+    printf("Enter hourly rate: ");
+    getline(&line, &buf_size, stdin);
+    hourly_rate = strtod(line, &line);
+    
+    printf("%s, %s: %.2lf\n", last_name, first_name, hours_worked*hourly_rate);
+
+    return 0;
 }
